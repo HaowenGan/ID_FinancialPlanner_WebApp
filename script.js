7d7aa376bf4aa25eb2514644954fc86500ca1409
HaowenGan <S10227840@connect.np.edu.sg>
2022-02-14 16:53:52 +0800

    Testing scripts

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let url = "https://www.tablebuilder.singstat.gov.sg/publicfacing/rest/timeseries/tabledata/17057"
 
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
